122c0fa4c9075f8546e35f366fe77a470b823aae
Anmol Rattan <anmolrattan@Anmols-MacBook-Air.local>
2018-08-14 23:59:47 +0530

    repocreate

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'aggregator'`, async(() => {
+  it(`should have as title 'Aggregator'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('aggregator');
+    expect(app.title).toEqual('Aggregator');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to aggregator!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Aggregator!');
   }));
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TwitterComponent } from './twitter/twitter.component';
+import { IeeeComponent } from './ieee/ieee.component';
+import { HackernewsComponent } from './hackernews/hackernews.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TwitterComponent,
+    IeeeComponent,
+    HackernewsComponent
   ],
   imports: [
     BrowserModule
