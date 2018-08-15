6f8d57fb0e7d73086512690217be9ea106982d1c
Anmol Rattan <anmolrattan@Anmols-MacBook-Air.local>
2018-08-16 00:14:00 +0530

    ResearchComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { error } from 'util';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
   title = 'Aggregator';
+  private apiUrl = 'https://api.elsevier.com/content/search/scidir';
+  data: any = {};
+
+    constructor(public http: HttpClient){
+      console.log('Hello Fellow User');
+    }
+    
+    getData(){
+const params = new HttpParams()
+.set('query', 'nodejs').set('apiKey', '810d0d9a08e546708564d462c1a5246d');
+
+      return this.http.get<any[]>(this.apiUrl, {params} ).subscribe(data => {
+        this.data = data;
+      },
+      error => {
+
+      }  
+    );
+  }
 }
+
