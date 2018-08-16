7297644627f0a95253db46ccf1e2b84ed2ef45bd
shivam <shivam.vishwakarma@xoriant.com>
2018-08-16 18:26:12 +0530

    Backend files

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+var appsettings = {
+    consumerkey: 'jSXiCVSp0mcWJJlUecrS1gXex',
+    consumersecret: 'mNbH4v7Aye8uGDLbaL14es5bbKx2B6lWJq7xvV1D4p6iUg7wRP',
+    bearertoken: ''
+};
+
+module.exports = appsettings;
