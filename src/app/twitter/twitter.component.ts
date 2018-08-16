6075aaae0576f05f6bbabf9bc5404ea93b055457
shivam <shivam.vishwakarma@xoriant.com>
2018-08-16 15:39:10 +0530

    Twitter final

--- src/app/twitter/twitter.component.ts
+++ src/app/twitter/twitter.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { DataService} from './../data.service';
+import { Http, Headers } from '@angular/http';
 
 @Component({
   selector: 'app-twitter',
   templateUrl: './twitter.component.html',
   styleUrls: ['./twitter.component.css']
 })
-export class TwitterComponent implements OnInit {
+export class TwitterComponent{
 
-  constructor() { }
+ 
+searchkey;
+tweetsdata;
+
+  constructor(private newService:DataService, private http: Http) { }
 
   ngOnInit() {
+  	this.searchkey = this.newService.getData();
+ 
+    var headers = new Headers();
+    
+    headers.append('Content-Type', 'application/X-www-form-urlencoded');
+    
+    this.http.post('http://localhost:3000/authorize', {headers: headers}).subscribe((res) => {
+      console.log(res);
+    })
+
+    var searchterm = 'query=' + this.searchkey;
+    
+    this.http.post('http://localhost:3000/search', searchterm, {headers: headers}).subscribe((res) => {
+      this.tweetsdata = res.json().data.statuses;
+    });
+ 
   }
+
 
 }
